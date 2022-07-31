e7d615b02a6be6b97301d8157d520b9357b61b3c
Edgar Selihov <edgar.selihov@gmail.com>
2022-07-31 20:14:57 +0300

    TUUM-002 - Add AccountServiceTest

--- src/main/java/com/edgsel/tuumtestassignment/controller/dto/response/AccountResponseDTO.java
+++ src/main/java/com/edgsel/tuumtestassignment/controller/dto/response/AccountResponseDTO.java
@@ -1,10 +1,16 @@
 package com.edgsel.tuumtestassignment.controller.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class AccountResponseDTO {
 
     private long id;
